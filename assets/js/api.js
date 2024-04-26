efc2750617820151e27352e6a53fb611dd2f1321
DiegoSCandido <diegoscandido19@gmail.com>
2024-04-25 21:22:56 -0300

    atualizando cabeçalho

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
-async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+async function fetchProfileData() {https
+    const url = '://https://raw.githubusercontent.com/DiegoSCandido/js-developer-portfolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
